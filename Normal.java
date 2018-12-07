b22e3d5e671a8df63f8efb3f164dd46e49fb1dd2
Siroyan <1517053@ed.tus.ac.jp>
2018-12-07 21:06:17 +0900

    [add] To make bin data

--- Normal.java
+++ Normal.java
@@ -1,10 +1,13 @@
 public class Normal {
     public static void main(String[] args){
-
+        char input[] = {'A','B','C','D','E','1','2','3'};
         int symbol[][] = new int[21][21];
+        
         QRCode qr = new QRCode();
-        symbol = qr.getSymbol();
+        qr.setMeta(1,1);
+        qr.convertBinary(input);
 
+        symbol = qr.getSymbol();
         for(int i=0; i < symbol.length; i++){
             for(int j=0; j < symbol[0].length; j++){
                 if(symbol[i][j] == 0) System.out.print("□ ");
--- QRCode.java
+++ QRCode.java
@@ -1,6 +1,8 @@
+import java.util.Arrays;
 class QRCode {
     private int symbol[][] = new int[21][21];
-    private int rawdata[] = new int[72];
+    private int binaryData[] = new int[72];
+    private int cursor = 0;
 
     public QRCode(){
         /* To create detection of position and timing pattern */
@@ -30,18 +32,40 @@ class QRCode {
         System.arraycopy(symbolBase,0,symbol,0,symbolBase.length);
     }
 
-    public void setMode(int modeIndicator){
+    public void setMeta(int modeIndicator, int errLvIndicator){
         int mode[] = {0,0,1,0};
-        System.arraycopy(mode,0,symbol,0,mode.length);
-    }
-    public void setErrLevel(int errLvIndicator){
+        System.arraycopy(mode,0,binaryData,0,mode.length);
+        cursor += mode.length;
+
         int errLv[] = {0,0,0,0,0,1,0,0,0};
-        System.arraycopy(errLv,0,symbol,0,errLv.length);
+        System.arraycopy(errLv,0,binaryData,cursor,errLv.length);
+        cursor += errLv.length;
     }
+    public void convertBinary(char rawdata[]){
+        for(int i = 0; i < rawdata.length; i += 2){
+            //int dec = 10 * 45 + 11;
+            int dec = value(rawdata[i]) * 45 + value(rawdata[i+1]);
+            int bin[] = {0,0,0,0,0,0,0,0,0,0,0};
+            for(int j = 0; dec > 0; j++){
+                bin[10 - j] = dec % 2;
+                dec = dec / 2;
+            }
+            System.arraycopy(bin,0,binaryData,cursor,bin.length);
+            cursor += bin.length;
+        }
+        System.out.println(Arrays.toString(binaryData));    //DEBUG
+    }
+
     public void setSymbolVersion(){
         /* To define Symbol Version */
     }
     public int[][] getSymbol(){
         return symbol;
+    }
+
+    private int value(char c){
+        if('A' <= c && c <= 'Z') return c - 'A' + 10;
+        if('0' <= c && c <= '9') return c - '0';
+        else return 0;     
     }
 }
